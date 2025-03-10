42623c5f6574a17b8463e42e51f1db5e8cf51f55
MdBillG <user.email>
2025-03-10 11:16:04 +0530

    working on pyramid paterns

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log('inde')
+
+let n =9
+// for(let i=0;i<=n;i++){
+//     let row =''
+// for(let j=1;j<=i;j++){
+//      row += "*"
+// }
+// console.log(row)
+// }
+
+// for(let i=5;i>=0;i--){
+//     let row =''
+// for(let j=1;j<=i;j++){
+//      row += "*"
+// }       
+// console.log(row)
+// }   
+
+// for(let i=1;i<=5;i++){
+//     let row= ""
+//     console.log(row)
+// }
+
+//pyramid
+
+// for(let i=1 ; i<=n ;i++){
+//     let row = ''
+//     for(let j=1;j<=n-i;j++){
+//         row += " "
+//     }
+//     for(let j=1;j<=(2*i)-1;j++){
+//         row += "*"
+//     }
+//     console.log(row)
+// }
+
+for (let i=1;i<=n;i++){
+    let row =""
+    for(let j=1;j<=i;j++){
+        row = "*"
+    }
+    console.log(row)
+}
